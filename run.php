33becc8f62c6e076172bd367649cda69969b788f
Arivan Ismail <187494087+arivanismail-05@users.noreply.github.com>
2025-05-19 22:08:03 +0300

    Add run.css for styling: implement layout, responsive design, and enhance user interface elements for the code editor.

--- run.php
+++ run.php
@@ -5,8 +5,6 @@
 require_once 'config.php';
 $id = $_GET['language_id'] ;
 $lecture_id = $_GET['lecture_id'] ;
-echo "language_id: " . htmlspecialchars($id) . "<br>";
-echo "lecture_id: " . htmlspecialchars($lecture_id) . "<br>";
 ?>
 
 <!DOCTYPE html>
@@ -17,10 +15,11 @@ echo "lecture_id: " . htmlspecialchars($lecture_id) . "<br>";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/run.css">
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-5">
         <div class="left">
             <div class="text_run">
                 <label for="">Code Editor</label>
